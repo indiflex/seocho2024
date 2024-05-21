57654ed0bd8200f3fa4abd464117026a62a16f3c
indiflex <indiflex1@gmail.com>
2024-05-21 10:42:07 +0900

    js init

---
+++ js/hoisting.js
@@ -0,0 +1,26 @@
+let i;
+var x = undefined;
+f = undefined;
+function ff() { console.log('ff>', y, yy); }
+var y = undefined;
+var xx = undefined;
+
+console.log(i); // ReferenceError: Cannot access 'i' before initialization
+i = 1;
+console.log('x=', x); // x= undefined
+x = 1;
+console.log(ff, f);   // [Function: ff], undefined
+f();      // TypeError: f is not a function
+{
+  function f() { console.log('f>', x, xx); }  // 여기서 <f.o> 할당!!
+  const b;
+  f();      // 1, undefined (:LexicalScope)
+  x = 2; b = 1;
+}
+if (x > 2) {
+  let yy = <unInitializedYet>;
+  y = 5;
+  yy = 55;
+}
+xx = 100;
+ff();  // ReferenceError: yy is not defined 
---
+++ js/operator.js
@@ -0,0 +1,6 @@
+// let a = 1, b = 2;
+let a = 1;
+let b = 2;
+
+const c = (a++, b++);
+console.log('c =', c, a, b);
