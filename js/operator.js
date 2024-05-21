d296851fe580c9f1c64ba044cb17b765fc68bfaf
indiflex <indiflex1@gmail.com>
2024-05-21 11:49:51 +0900

    oper

--- js/operator.js
+++ js/operator.js
@@ -2,5 +2,32 @@
 let a = 1;
 let b = 2;
 
-const c = (a++, b++);
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+let c = (a++, b++);
+// const c = ((a = a + 1), (b = b + 1));
 console.log('c =', c, a, b);
+
+let d = (a--, b + a);
+console.log('d =', d, a, b);
+
+d = void (c = a + b);
+console.log(a, b, c, d); // ?
+
+console.log('-------------------');
+const first = 'Uncle';
+const last = 'Bob';
+if (first) {
+  console.log(first);
+}
+console.log(last);
+
+let fullName = first ? first + ' ' : '';
+//         'Uncle ' + 'Bob'
+//            ''    + 'Bob'
+fullName = fullName + last;
+console.log('fullName =', fullName);
+
+console.log(`${first}${first ? ' ' : ''}${last}`);
