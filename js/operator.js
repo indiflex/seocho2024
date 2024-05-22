db7f418fe2aaa4bacb629da75fd25e0eb610a63e
indiflex <indiflex1@gmail.com>
2024-05-22 11:51:56 +0900

    if

--- js/operator.js
+++ js/operator.js
@@ -7,6 +7,11 @@ let b = 2;
 // }
 
 let c = (a++, b++);
+// ==> a = a + 1; let c = b; b = b + 1;
+
+// let c = (a++, ++b);
+// ==> a + a + 1; b = b + 1; let c = b;
+
 // const c = ((a = a + 1), (b = b + 1));
 console.log('c =', c, a, b);
 
@@ -31,3 +36,89 @@ fullName = fullName + last;
 console.log('fullName =', fullName);
 
 console.log(`${first}${first ? ' ' : ''}${last}`);
+
+console.log('-------------------');
+const bi = 0b101;
+const oct = 0o101;
+const hex = 0x101;
+console.log(bi, oct, hex);
+console.log(parseInt('101', 36));
+console.log(parseFloat('10.100'));
+console.log(Number('10.100'));
+
+console.log(-Infinity);
+console.log(isNaN(Infinity));
+console.log(Number.MAX_VALUE);
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
+console.log('----------------------');
+console.log(9007199254740991 === 9007199254740991);
+console.log(9007199254740991 === 9007199254740992); // false
+console.log(9007199254740993 === 9007199254740992);
+console.log(9007199254740993n === 9007199254740992n);
+
+console.log(Number.isInteger(0.1));
+
+const num = 123.456;
+console.log(num.toFixed(2));
+console.log(num, Math.trunc(Number(num.toFixed(2)) + 9));
+
+const three = 0.1 + 0.2;
+console.log(three);
+console.log(0.1 + 0.7);
+console.log(0.3 + 0.2);
+const ep = Math.abs(three - 0.3) < Number.EPSILON;
+console.log('🚀  ep:', ep, Number.EPSILON);
+console.log(Math.trunc((0.1 + 0.2) * 10) / 10);
+console.log(2.5 % 1);
+console.log(2.4 % 1);
+
+console.log('===========================');
+if (three === 0.3) {
+  console.log('OK');
+} else {
+  console.log('exists garbage!');
+}
+
+const x = 2;
+// x가 1이면 one, 2면 two, 3이면 three, 그 외 etc
+
+// 1. if 문
+if (x === 1) {
+  console.log('one');
+} else if (x === 2) {
+  console.log('two');
+} else if (x === 3) {
+  console.log('three');
+} else {
+  console.log('etc');
+}
+
+let outStr = 'etc';
+if (x === 1) {
+  outStr = 'one';
+} else if (x === 2) {
+  outStr = 'two';
+} else if (x === 3) {
+  outStr = 'three';
+}
+console.log(outStr);
+
+// 2. switch 문
+switch (x) {
+  case 1:
+    console.log('one');
+    break;
+  case 2:
+    console.log('two');
+    break;
+  case 3:
+    console.log('three');
+    break;
+  default:
+    console.log('etc');
+}
+
+// 3. 3항 연산자
+outStr = x === 1 ? 'one' : x === 2 ? 'two' : x === 3 ? 'three' : 'etc';
+console.log('🚀  outStr:', outStr);
