f084f1f48e5712827963b7fee63710c821fbf347
indiflex <indiflex1@gmail.com>
2024-06-03 14:44:06 +0900

    iterator & generator

---
+++ js/iter_gener.js
@@ -0,0 +1,42 @@
+const obj = {
+  values: [1, 2, 3, 4, 5],
+
+  // [Symbol.iterator]() {
+  //   // iterator를 반환한다.
+  //   const self = this;
+  //   return {
+  //     currIdx: -1, // current cursor(index)
+  //     next() {
+  //       this.currIdx += 1;
+  //       return {
+  //         value: self.values[this.currIdx],
+  //         done: this.currIdx >= self.values.length,
+  //       };
+  //     },
+  //   };
+  // },
+
+  *[Symbol.iterator]() {
+    for (const a of this.values) yield a;
+  },
+
+  iterator() {
+    return this[Symbol.iterator]();
+  },
+};
+console.log('🚀>>  obj:', obj.values);
+
+const it = obj.iterator();
+console.log('🚀>>  obj.iterator:', it);
+console.log('🚀>>  next:', it.next());
+console.log('🚀>>  next:', it.next());
+console.log('🚀>>  next:', it.next());
+console.log('🚀>>  next:', it.next());
+console.log('🚀>>  next:', it.next());
+console.log('🚀>>  next:', it.next());
+
+console.log('---------------------');
+console.log('XXX=', [...obj]);
+for (const x of obj) {
+  console.log(x);
+}
---
+++ js/oop.js
@@ -0,0 +1,84 @@
+// function Aminal(name) { console.log('aaa', name)}
+// new Aminal()
+class Animal {
+  static ID = 1;
+  name;
+  constructor(name) {
+    this.name = name;
+    console.log('==> Born!!', this.name);
+  }
+
+  bark() {
+    console.log('bark!!!');
+  }
+}
+
+const ani = new Animal('Ani');
+ani.bark();
+
+class Dog extends Animal {
+  // overriding
+  bark() {
+    console.log('Bow wow!!!');
+  }
+}
+
+class Cat extends Animal {
+  #age = 10; // private
+  bark() {
+    console.log('miaow~', this.#age, this.name);
+  }
+
+  getAge() {
+    return this.#age;
+  }
+
+  set age(_age) {
+    this.#age = _age;
+  }
+
+  get age() {
+    return this.#age;
+  }
+
+  [Symbol.iterator]() {
+    return this.name.split('').values();
+  }
+
+  values() {
+    return this[Symbol.iterator]();
+  }
+}
+
+const lucy = new Dog('Lucy');
+lucy.bark();
+
+const nabi = new Cat('Nabi');
+nabi.bark();
+console.log('🚀>>  nabi.name:', nabi.name);
+nabi.age = 12;
+console.log('🚀-->>  nabi.age:', nabi.age); // age()
+console.log('🚀-->>  nabi.getAge:', nabi.getAge());
+
+// speak는 Animal 만 받을 수 있다! (다형성)
+function speak(animal) {
+  animal.bark();
+}
+
+speak(nabi);
+speak(lucy);
+speak(ani);
+
+console.log('---------------------');
+//       instance    class
+console.log(ani.ID, Animal.ID, ani.toString());
+
+// ex. Animal을 상속받은 Pig 클래스를 작성하시오.
+class Pig extends Animal {
+  bark() {
+    console.log('꿀! 꿀!');
+  }
+}
+
+const gg2 = new Pig('꿀꿀이');
+gg2.bark();
