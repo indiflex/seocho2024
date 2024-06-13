47dd7c7dc82da7a6b10819492cbc230b61e5cae1
indiflex <indiflex1@gmail.com>
2024-06-13 10:18:34 +0900

    class

--- ts/hightype.ts
+++ ts/hightype.ts
@@ -20,3 +20,45 @@ type Ud2 = (User | Dept) & { addr: string };
 // 다음 코드가 오류가 없으면 통과!
 const ud2: Ud2 = { id: 1, name: 'HH', addr: 'Seoul' };
 const ud3: Ud2 = { id: 1, dname: 'HH', captain: 'HH', addr: 'Seoul' };
+
+// ---------------- class
+interface Animal {
+  move(): void;
+  // move: () => void;
+}
+
+class Pet implements Animal {
+  name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  move(): void {
+    console.log('Pet is moving!!');
+  }
+}
+
+class Dog extends Pet {
+  bark() {
+    console.log('멍멍!', this.name);
+  }
+  howling() {
+    console.log('아~오~', this.name);
+  }
+}
+class Cat extends Pet {
+  kukuki() {
+    console.log('꾹!꾹!');
+  }
+  bark() {
+    console.log('야옹!', this.name);
+  }
+}
+
+const lucy = new Dog('Lucy');
+lucy.bark();
+const maxx = new Dog('Max');
+maxx.howling();
+
+const happy = new Cat('Happy');
+happy.bark();
+happy.kukuki();
