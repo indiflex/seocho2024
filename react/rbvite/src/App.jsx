ac173280d059badd8f46b746de96a3775b879a4a
indiflex <indiflex1@gmail.com>
2024-06-19 11:43:16 +0900

    hello - login

--- react/rbvite/src/App.jsx
+++ react/rbvite/src/App.jsx
@@ -3,15 +3,21 @@ import './App.css'
 import Hello from './components/Hello';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [didLogin, setDidLogin] = useState(true);
   const plusCount = () => setCount(count + 1);
 
+  const toggleLogin = () => {
+    setDidLogin(!didLogin);
+  }
   return (
     <>
       <div>
-        <Hello name="Jade" age={33} plusCount={plusCount} />
+        {didLogin && <Hello name="Jade" age={33} plusCount={plusCount} />}
       </div>
-      <button onClick={plusCount}>plus count</button>
+      <button onClick={toggleLogin}>
+        Toggle {didLogin ? 'Logined' : 'NotLogined'}
+      </button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
