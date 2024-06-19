e46a7a7034742cbbfc3c448fa9063faaa0ef2b08
indiflex <indiflex1@gmail.com>
2024-06-19 15:47:37 +0900

    before git flow

--- react/rbvite/src/components/Hello.jsx
+++ react/rbvite/src/components/Hello.jsx
@@ -2,5 +2,5 @@
 export default function Hello(props) {
   return <h1 onClick={props.plusCount} style={{
   cursor: 'pointer'
-}}>Hello, {props.name}! <small className="font-sm">({props.age})</small></h1>
+}}>Hello, {props.name}! <small className="font-sm text-red-500">({props.age})</small></h1>
 }
