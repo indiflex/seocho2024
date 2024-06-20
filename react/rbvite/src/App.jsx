bf58d2a882c4ec9c1f7528a2323b393d31112803
indiflex <indiflex1@gmail.com>
2024-06-20 10:20:33 +0900

    session

--- react/rbvite/src/App.jsx
+++ react/rbvite/src/App.jsx
@@ -2,22 +2,42 @@ import { useState } from 'react'
 import './App.css'
 import Hello from './components/Hello';
 
+// mock
+const SampleSession = {
+  loginUser: { id: 1, name: 'Hong', age: 33 },
+  cart: [
+    { id: 100, name: '라면', price: 3000 },
+    { id: 101, name: '컵라면', price: 2000 },
+    { id: 200, name: '파', price: 5000 },
+  ],
+};
+
 function App() {
+  const [session, setSession] = useState(SampleSession);
   const [count, setCount] = useState(0);
-  const [didLogin, setDidLogin] = useState(true);
+  // const [didLogin, setDidLogin] = useState(true);
   const plusCount = () => setCount(count + 1);
 
-  const toggleLogin = () => {
-    setDidLogin(!didLogin);
+  // console.log('Appppppppppppp!', count)
+
+  // const toggleLogin = () => {
+  //   setDidLogin(!didLogin);
+  // }
+
+  const logout = () => {
+    // session.loginUser = null;
+    setSession({ ...session, loginUser: null });
   }
+
   return (
     <>
       <div>
-        {didLogin && <Hello name="Jade" age={33} plusCount={plusCount} />}
+        {session.loginUser && <Hello name={session.loginUser.name} age={session.loginUser.age} plusCount={plusCount} />}
       </div>
-      <button onClick={toggleLogin}>
-        Toggle {didLogin ? 'Logined' : 'NotLogined'}
-      </button>
+      {/* <button onClick={toggleLogin}>
+        Toggle {session.loginUser ? 'Logined' : 'NotLogined'}
+      </button> */}
+      <button onClick={logout} className='ml-3'>SignOut</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
--- react/rbvite/src/components/Hello.jsx
+++ react/rbvite/src/components/Hello.jsx
@@ -1,6 +1,7 @@
 
 export default function Hello(props) {
+  // console.log('Heeeeeeeeelo')
   return <h1 onClick={props.plusCount} style={{
   cursor: 'pointer'
-}}>Hello, {props.name}! <small className="font-sm text-red-500">({props.age})</small></h1>
+}}>Hello, {props.name}! <small className="font-sm text-red-500">({props.age + 1})</small></h1>
 }
