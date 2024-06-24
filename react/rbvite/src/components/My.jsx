fb62ff20f15dc268a7a5f8c3bc067f953bb9dcb7
indiflex <indiflex1@gmail.com>
2024-06-24 09:21:38 +0900

    tw prettier

--- react/rbvite/src/components/My.jsx
+++ react/rbvite/src/components/My.jsx
@@ -1,20 +1,29 @@
 import Login from "./Login";
 import Profile from "./Profile";
+import Button from "./atoms/Button";
 
-export default function My({session: {loginUser, cart}, signOut}) {
-  return <>
-    {loginUser ? <Profile name={loginUser?.name} signOut={signOut} />
-               : <Login />}
-    
-    <div className="border mt-5">
-      <ul>
-        {cart.map(item => (
-          <li key={item.id}>
-            {item.name}
-            <small className="text-gray-300 ml-2">({item.price.toLocaleString()}원)</small>
-          </li>
-        ))}
-      </ul>
-    </div>
-  </>;
+export default function My({ session: { loginUser, cart }, signOut }) {
+  return (
+    <>
+      {loginUser ? (
+        <Profile name={loginUser?.name} signOut={signOut} />
+      ) : (
+        <Login />
+      )}
+
+      <div className="mt-5 border">
+        <ul>
+          {cart.map((item) => (
+            <li key={item.id}>
+              {item.name}
+              <small className="ml-2 p-3 pl-2 pr-2 text-gray-300">
+                ({item.price.toLocaleString()}원)
+              </small>
+            </li>
+          ))}
+        </ul>
+        <Button text="상품추가" />
+      </div>
+    </>
+  );
 }
