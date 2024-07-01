f73c75dcf3bc0832eac192052c1721085143cd8e
indiflex <indiflex1@gmail.com>
2024-07-01 09:47:51 +0900

    storybook input

--- react/rbvite/src/components/atoms/Input.stories.jsx
+++ react/rbvite/src/components/atoms/Input.stories.jsx
@@ -12,4 +12,43 @@ export const DefaultInput = {
   },
 };
 
+export const PasswordInput = {
+  args: {
+    label: "PasswordInput",
+    type: "password",
+  },
+};
+
+export const EmailInput = {
+  args: {
+    label: "EmailInput",
+    type: "email",
+  },
+};
+
+export const NumberInput = {
+  args: {
+    label: "NumInput",
+    type: "number",
+  },
+};
+
+export const DateInput = {
+  args: {
+    label: "DateInput",
+    type: "date",
+  },
+};
+
+export const Others = {
+  render: () => (
+    <>
+      <Input type="text" label="ID" placehoder="UserID..." />
+      <Input type="password" label="Password" placehoder="UserPassword..." />
+      <Input type="tel" label="전화번호" />
+      <Input type="color" label="Color" />
+    </>
+  ),
+};
+
 export default meta;
