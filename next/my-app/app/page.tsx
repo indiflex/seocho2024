259f7b1af9d24b4345f4eb7040c9c3874ba93da4
indiflex <indiflex1@gmail.com>
2024-07-08 13:06:59 +0900

    vercel

--- next/my-app/app/page.tsx
+++ next/my-app/app/page.tsx
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+// import Image from 'next/image';
 import Link from 'next/link';
 
 export default function Home() {
@@ -7,14 +7,14 @@ export default function Home() {
       <Link href='/hello'>Hello</Link>
       <Link href='/hi'>Hi~</Link>
       <Link href='/about'>About</Link>
-      <Image
+      {/* <Image
         src='/vercel.svg'
         alt='Vercel Logo'
         className='dark:invert'
         width={100}
         height={24}
         priority
-      />
+      /> */}
     </main>
   );
 }
