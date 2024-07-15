7d00b24ff36f80ea25a413d64f8c674179497794
indiflex <indiflex1@gmail.com>
2024-07-15 15:45:57 +0900

    insert book

--- next/bookmark/app/api/books/route.ts
+++ next/bookmark/app/api/books/route.ts
@@ -1,17 +1,30 @@
+import { query } from '@/lib/db';
 import { NextRequest, NextResponse } from 'next/server';
-import mysql from 'mysql2/promise';
-import { config } from '@/lib/db';
+
+export async function POST(req: NextRequest) {
+  const { title, owner, clickdel } = await req.json();
+  const [rows] = await query(
+    'insert into Book(title, owner, clickdel) values(?,?,?)',
+    [title, owner, clickdel]
+  );
+
+  return NextResponse.json({ rows });
+}
 
 export async function GET(req: NextRequest) {
   const { searchParams } = req.nextUrl;
   const userId = searchParams.get('userId');
   console.log('🚀  userId:', userId);
-  const conn = await mysql.createConnection(config);
-  const [books, fields] = await conn.query(
-    'select * from Book where owner=' + userId
-  );
-  conn.end();
-  console.log('🚀  rows:', books);
-  console.log('🚀  fields:', fields);
-  return NextResponse.json({ userId, message: 'OK', books });
+  // const conn = await mysql.createConnection(config);
+  // const conn = await pool.getConnection();
+  try {
+    const [books] = await query('select * from Book where owner = ?', [userId]);
+
+    return NextResponse.json({ userId, message: 'OK', books });
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'error!';
+    return NextResponse.json({ userId, message }, { status: 500 });
+  }
+  // conn.end(); // close
+  // conn.release(); // release to pool
 }
--- next/bookmark/lib/db.ts
+++ next/bookmark/lib/db.ts
@@ -1,8 +1,34 @@
-export const config = {
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_SCHEMA,
+import mysql from 'mysql2/promise';
+
+const {
+  DB_HOST: host,
+  DB_USER: user,
+  DB_PASS: password,
+  DB_SCHEMA: database,
+} = process.env;
+
+const config = {
+  host,
+  user,
+  password,
+  database,
   waitForConnections: true,
+  connectionLimit: 3,
+  maxIdle: 3,
+  idleTimeout: 60000,
 };
-console.log('🚀  config:', config);
+// console.log('🚀  config:', config);
+const pool = mysql.createPool(config);
+
+export const query = async (sql: string, params: unknown[]) => {
+  const conn = await pool.getConnection();
+  try {
+    const [rows, fields] = await conn.query(sql, params);
+    return [rows, fields];
+  } catch (error) {
+    console.table({ error });
+    throw error;
+  } finally {
+    conn.release();
+  }
+};
